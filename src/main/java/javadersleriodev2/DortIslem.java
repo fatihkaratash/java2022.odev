098e07b34862a4cc3171630e72d607642d7d88c9
Fatih Karataş <f.karatash.dev@gmail.com>
2022-10-03 13:48:24 +0300

    Homework

---
+++ src/main/java/javadersleriodev2/DortIslem.java
@@ -0,0 +1,24 @@
+package javadersleriodev2;
+
+public class DortIslem {
+
+    public int Topla(int num1, int num2){
+        return num1+num2;
+    }
+
+    public int Cikar(int num1, int num2){
+        return num1-num2;
+    }
+
+    public int Carp(int num1, int num2){
+        return num1*num2;
+    }
+
+    public int Bol(int num1, int num2){
+        return num1+num2;
+    }
+
+
+
+
+}
---
+++ src/main/java/javadersleriodev2/ReCapDemo_Classes.java
@@ -0,0 +1,14 @@
+package javadersleriodev2;
+
+public class ReCapDemo_Classes {
+    public static void main(String[] args) {
+
+        // Kullanacagımız methodların Referans'ını oluşturuyoruz.
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.Topla(4,5);
+
+        System.out.println(sonuc);
+
+
+    }
+}
